4ae81c55c9b4aa182b6dff1f78cc7431cfaabab9
B70718 <wgrroseberry@fullsail.edu>
2013-11-28 08:36:35 -0700

    working on home page javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-// JavaScript Document
+$('#home').on('pageinit', function(){
+     console.log("the home page has fired");
+
+$( document ).on( "click", ".show-page-loading-msg", function() {
+     console.log("The on click loading message has fired");
+    var $this = $( this ),
+        theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+        msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
+        textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+        textonly = !!$this.jqmData( "textonly" );
+        html = $this.jqmData( "html" ) || "";
+    $.mobile.loading( "show", {
+            text: msgText,
+            textVisible: textVisible,
+            theme: theme,
+            textonly: textonly,
+            html: html
+    });
+})
+.on( "click", ".hide-page-loading-msg", function() {
+    $.mobile.loading( "hide" );
+});
+
+
+
+
+
+
+
+});

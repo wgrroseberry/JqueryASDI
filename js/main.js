7ea3c472098d5a4a0c05e900142c38574a42117a
B70718 <wgrroseberry@fullsail.edu>
2013-11-29 20:41:56 -0700

    Working  on the jquery validation and local storage

--- js/main.js
+++ js/main.js
@@ -16,11 +16,69 @@ $( document ).on( "click", ".show-page-loading-msg", function() {
             textonly: textonly,
             html: html
     });
+	
+	// Get a handle on the first button element in the document.
+	console.log("above the button querySelector line 21");
+    var button = document.querySelector( "button" );
+	console.log("below the button selector line 23");
+	 // If a user clicks on it, say hello!
+	 console.log("above say hello function");
+    button.addEventListener( "click", function( ev ) {
+        alert( "Hello" );
+    }, false);
 })
 .on( "click", ".hide-page-loading-msg", function() {
     $.mobile.loading( "hide" );
 });
 
+$('#sign').on('pageinit', function(){
+ console.log(" The page signup has fired on line 35!!");
+ 
+ $('#recordsignuphere').submit(function() {
+ console.log("inside submit funtion like 38");
+  alert("The form has been submitted");
+ });
+ 
+ 
+  
+ var storeData = function(key) {
+ 
+ if (!key) {
+ console.log("makeing a key");
+ var id = Math.floor(Math.random()*100000000001);
+ console.log("Here is the new key" + id );
+ }else {
+ 	// Use the same key if one is already assigned 
+ 	id = key;
+ 	console.log("key is already assigned"+ key );
+  }
+ 	// function not made getItems();
+ 	var item         = {};
+ 	    item.firstname         = ["First Name:", $('#firstName').val()];
+ 	    console.log( firstName );
+ 	
+ 	//Save to Local Storage
+ 	localStorage.setItem(id, JSON.stringify(item));
+ 	alert("Your faviorate Paint by number is saved!");
+ 
+ 
+ }
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+});
 
 
 
